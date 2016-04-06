6ca491c949a3ba9a8e2450bf915ebef93f6f5d31
Seab <honeyboyseab@gmail.com>
2016-04-06 15:16:31 -0400

    added localized strings for the spanish language

--- WorldTrotter/WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/WorldTrotter/MapViewController.swift
@@ -20,7 +20,11 @@ class MapViewController: UIViewController {
         // Set it as *the* view of this view controller
         view = mapView
         
-        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        // let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        let standardString = NSLocalizedString("Standard", comment: "Standard map view")
+        let satelliteString = NSLocalizedString("Satellite", comment: "Satellite map view")
+        let hybridString = NSLocalizedString("Hybrid", comment: "Hybrid map view")
+        let segmentedControl = UISegmentedControl(items: [standardString, satelliteString, hybridString])
         segmentedControl.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
         segmentedControl.selectedSegmentIndex = 0
         
