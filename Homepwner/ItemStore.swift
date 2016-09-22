a65549c927b583b9a7b544e60566502d9e095885
Seab <honeyboyseab@gmail.com>
2016-09-22 03:09:06 -0400

    implemented tableView data source and editing features as well as alert views

--- Homepwner/ItemStore.swift
+++ Homepwner/ItemStore.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ItemStore {
     
-    var allItems = [Item()]
+    var allItems = [Item]()
     
     func createItem() -> Item {
         let newItem = Item(random: true)
@@ -19,9 +19,25 @@ class ItemStore {
         return newItem
     }
     
-    init() {
-        for _ in 0..<5 {
-            createItem()
+    func removeItem(item: Item) {
+        if let index = allItems.indexOf(item) {
+            allItems.removeAtIndex(index)
         }
+    }
+    
+    func moveItemAtIndex(fromIndex: Int, toIndex: Int) {
+        if fromIndex == toIndex {
+            return
+        }
+        
+        // Get reference to object being moved so you can reinsert it
+        let movedItem = allItems[fromIndex]
+        
+        // remove item from array
+        allItems.removeAtIndex(fromIndex)
+        
+        // insert item in array at new location
+        allItems.insert(movedItem, atIndex: toIndex)
+        
     }
 }
--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -12,6 +12,36 @@ class ItemsViewController: UITableViewController {
     
     var itemStore: ItemStore!
     
+    @IBAction func addNewItem(sender: AnyObject) {
+        // create a new item and add it to the store
+        let newItem = itemStore.createItem()
+        
+        // figure out where that item is in the array
+        if let index = itemStore.allItems.indexOf(newItem) {
+            let indexPath = NSIndexPath(forRow: index, inSection: 0)
+            
+            // insert this new row into the table
+            tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+        }
+    }
+    
+    @IBAction func toggleEditingMode(sender: AnyObject) {
+        // If you are currently in editing mode ...
+        if editing {
+            // change text of button to inform user of state
+            sender.setTitle("Edit", forState: .Normal)
+            
+            // Turn off editing mode
+            setEditing(false, animated: true)
+        } else {
+            // Change text of button to inform user of state
+            sender.setTitle("Done", forState: .Normal)
+            
+            // Enter editing mode
+            setEditing(true, animated: true)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -35,10 +65,59 @@ class ItemsViewController: UITableViewController {
         // this cell will appear in the tableview
         let item = itemStore.allItems[indexPath.row]
         cell.textLabel?.text = item.name
-        
         cell.detailTextLabel?.text = "$\(item.valueInDollars)"
-    
-        
         return cell
     }
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        // if the table view is asking to commit a delete command ...
+        if editingStyle == .Delete {
+            let item = itemStore.allItems[indexPath.row]
+            
+            let title = "Delete \(item.name)?"
+            let message = "Are you sure you want to delete this item?"
+            
+            let ac = UIAlertController(title: title, message: message, preferredStyle: .ActionSheet)
+            let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+            ac.addAction(cancelAction)
+            
+            let deleteAction = UIAlertAction(title: "Delete", style: .Destructive, handler: { (action) -> Void in
+                // remove the item from the store
+                self.itemStore.removeItem(item)
+                
+                // also remove that row from the table view with an animation
+                self.tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+            })
+            ac.addAction(deleteAction)
+            
+            // present the alert controller
+            presentViewController(ac, animated: true, completion: nil)
+        }
+    }
+    
+    override func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+        // update the model
+        itemStore.moveItemAtIndex(sourceIndexPath.row, toIndex: destinationIndexPath.row)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
